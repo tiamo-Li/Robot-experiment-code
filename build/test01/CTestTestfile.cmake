8468780a39c40cc0ee6b76aca58428dff705dafc
tiamo <tiamo209@163.com>
2024-11-10 23:35:07 +0800

    README.md update

--- build/test01/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tiamo/first_ws/src/test01
-# Build directory: /home/tiamo/first_ws/build/test01
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
